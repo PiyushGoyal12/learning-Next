b3226b3f7cc97e4b00d8c7234f30e5671a3a1120
Piyush Goyal <2004piyushgoyal@gmail.com>
2025-02-23 18:56:07 +0530

    error.tsx

--- src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,10 +1,20 @@
+import { error } from "console";
 import { notFound, redirect } from "next/navigation";
+import { throwDeprecation } from "process";
+
+function getRandomInt(count: number) {
+    return Math.floor(Math.random() * count);
+}
 
 export default async function ReviewDetails({
     params,
 }: {
     params: Promise<{ productId: string; reviewId: string }>;
 }) {
+    const random = getRandomInt(2);
+    if (random === 1) {
+        throw new Error("error loading review");
+    }
     const { productId, reviewId } = await params;
     if (parseInt(reviewId) > 1000) {
         redirect("/products");
